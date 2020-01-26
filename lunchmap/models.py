81440d5e775cba3367c70942e268f9ca7c8f29b9
Eiichi Mano <eiichi.mano@gmail.com>
2020-01-26 12:01:06 +0900

    20200126

--- lunchmap/models.py
+++ lunchmap/models.py
@@ -33,4 +33,17 @@ class Shop(models.Model):
     def get_absolute_url(self):
         return reverse('lunchmap:detail', kwargs={'pk': self.pk})
     
+class Customer(models.Model):
+    name = models.CharField('名前', max_length=20)
+    address = models.CharField('住所', max_length=50)
+    lat = models.DecimalField('緯度', max_digits=8, decimal_places=6)
+    lng = models.DecimalField('経度', max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return str(self.name)
+    class Meta:
+        verbose_name = '顧客'
+        verbose_name_plural = '顧客'
+        
+    
     
